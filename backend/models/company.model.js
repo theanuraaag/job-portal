90c72c163470cb8e15741df573695e23086ba140
Anurag Bhardwaj <theanuraaag@gmail.com>
2024-07-29 18:11:25 +0530

    backend setup 1

---
+++ backend/models/company.model.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+const companySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String
+    },
+    website:{
+        type:String
+    },
+    location:{
+        type:String
+    },
+    logo:{
+        type:String,
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    }
+},{timestamps:true});
